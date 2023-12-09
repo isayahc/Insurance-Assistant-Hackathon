0c1638dce74be9d618bb948b30eff225724d991b
isayhc <isayahculbertson@gmail.com>
2023-12-09 09:25:59 -0500

    made a ingest file

--- ingest.py
+++ ingest.py
@@ -9,3 +9,24 @@ service_context = ServiceContext.from_defaults(text_splitter=text_splitter)
 index = VectorStoreIndex.from_documents(
     documents, service_context=service_context
 )
+
+from llama_index.query import QueryBuilder
+
+# Define the query text
+query_text = "How does the weather affect crop growth?"
+
+# Preprocess the query text
+query_builder = QueryBuilder(service_context)
+query = query_builder.build_query(query_text)
+
+# Search for similar documents or retrieve relevant information
+results = index.search(query)
+
+# Process the search results
+for result in results:
+    document_id = result.document_id
+    score = result.score
+    document = documents[document_id]
+    # Process the retrieved document or display the relevant information
+    print(f"Document ID: {document_id}, Score: {score}")
+    print(f"Document Text: {document.text}")
