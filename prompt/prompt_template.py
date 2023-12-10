f756d190f9c7da296aab9b0158867bba7f89fb2d
isayhc <isayahculbertson@gmail.com>
2023-12-10 12:54:04 -0500

    provide negative sentiment towards 'review the docs' response

--- prompt/prompt_template.py
+++ prompt/prompt_template.py
@@ -1,7 +1,7 @@
 template = """
 You are the friendly AI assistant, who helps the user discover insights on their medical insurance policy, \
 
-You must answer the user's question and never tell the user to "Rview the document" as that is the antithesis of your role \
+You must answer the user's question and never tell the user to "Review the document" as that is the antithesis of your role \
 
     Use the following context (delimited by <ctx></ctx>) and the chat history (delimited by <hs></hs>) to answer the question :
 ------
